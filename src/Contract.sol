0228590ce09a47179cdb7b43038dd1a7fc6810c4
DefiCake <reche@0xb.one>
2022-06-01 10:30:41 +0200

    Added some dev automation, fork capabilities

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-contract Contract {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Contract is ERC20("Test", "Test") {
+    function mint(uint256 amount) external {
+        _mint(msg.sender, amount);
+    }
+}
